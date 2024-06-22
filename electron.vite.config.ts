c5b7b0bf0257bec37816e3420cd3d5955bb4fdf0
Joao Pedro A <jaaopbr@gmail.com>
2024-06-21 22:07:47 -0300

    feat: add tailwind and some changes

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,15 +4,29 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        '@/lib': resolve('src/main/lib'),
+        '@shared': resolve('src/shared')
+      }
+    }
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    assetsInclude: 'src/renderer/assets/**',
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@shared': resolve('src/shared'),
+        '@/hooks': resolve('src/renderer/hooks'),
+        '@/assets': resolve('src/renderer/assets'),
+        '@/store': resolve('src/renderer/store'),
+        '@/components': resolve('src/renderer/hooks'),
+        '@/utils': resolve('src/renderer/utils'),
+        '@/mocks': resolve('src/renderer/mocks')
       }
     },
     plugins: [react()]
